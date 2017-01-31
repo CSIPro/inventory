d77e1559120c97521850a6750dd7b68af6119dd9
Erick <e_delfin_23@yahoo.com>
2017-01-30 23:12:43 -0700

    :goat: + Added detail view function for items and referenced its urlpattern. + Added 'inventory' namespace to make reference from templates, etc.

--- mysite/inventory/urls.py
+++ mysite/inventory/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'inventory'
+
 urlpatterns = [
 
     # /inventory/
     url(r'^$', views.index, name='index'),
+
+    # /inventory/<item_id>
+    url(r'^(?P<pk>[0-9]+)/$', views.detail, name='detail'),
 
 ]
--- mysite/inventory/views.py
+++ mysite/inventory/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from .models import Item
 
@@ -8,3 +9,17 @@ def index(request):
     context_dict = {'items': item_list}
 
     return render(request, 'inventory/item_list.html', context=context_dict)
+
+
+# pk = animal_id
+def detail(request, pk):
+    try:
+        item = Item.objects.get(pk=pk)
+    except Item.DoesNotExist:
+        raise Http404('Item no existe.')  # TODO: Planning on redirecting to custom 404 page in future
+
+    context = {
+        'item': item,
+    }
+
+    return render(request, 'inventory/item_detail.html', context)
