5b3df1f612d6f5bd248c1dd2d1ca52ffc0246f92
Erick <e_delfin_23@yahoo.com>
2017-01-30 22:31:38 -0700

    Fixed shit with item index view and url reference.

--- mysite/inventory/urls.py
+++ mysite/inventory/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^item_list$', views.item_list, name='item_list'),
+
+    # /inventory/
+    url(r'^$', views.index, name='index'),
+
 ]
--- mysite/inventory/views.py
+++ mysite/inventory/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-import os
-from models import Item
+from .models import Item
 
 
-def item_list(request):
-    item_list = Item.objects.order_by()
+# For /inventory/
+def index(request):
+    item_list = Item.objects.all()
     context_dict = {'items': item_list}
+
     return render(request, 'inventory/item_list.html', context=context_dict)
