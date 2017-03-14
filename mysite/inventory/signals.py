4dec40263e45038584b57ee2a4733a333de72458
Erick <e_delfin_23@yahoo.com>
2017-03-14 00:15:48 -0700

    + Added UserProfile for users to upload profile pic. + Made corresponding signal to create UserProfile object post_save to User obj. + Added get_directory_path func to get path for user to upload pic to.

--- mysite/inventory/signals.py
+++ mysite/inventory/signals.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import Item, IndividualItem
+from .models import Item, IndividualItem, UserProfile
 
 
 # Creates a new IndividualItem object whenever an Item object is created.
@@ -14,3 +15,16 @@ def create_individual_item(sender, instance, created, **kwargs):
         # Creates a new IndividualItem object for total items available.
         for item in range(total):
             IndividualItem.objects.create(item=instance)
+
+
+# -----------------------------------------------------------------------
+# User shit that can be moved to another app.
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
