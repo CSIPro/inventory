b4555f1242cc21bfd96184646d275f5e76f6bb9e
Erick <e_delfin_23@yahoo.com>
2017-04-06 01:24:47 -0700

    Added dj_static to serve media files and uploads from Django.

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -11,8 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling, MediaCling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-application = get_wsgi_application()
+application = Cling(MediaCling(get_wsgi_application()))
 application = DjangoWhiteNoise(application)
